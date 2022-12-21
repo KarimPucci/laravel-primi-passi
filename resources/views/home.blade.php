855754d4a8de0127d516e54e05a3324511365ce6
Karim <kizpucci@gmail.com>
2022-12-21 17:48:07 +0100

    finish

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,11 +5,29 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     <title>Laravel primi passi</title>
 </head>
 
 <body>
     <h1> {{ $saluto }} </h1>
+
+    @if ($stampa_immagine)
+        <div class="img">
+            <img src="https://pbs.twimg.com/profile_images/587949417577066499/3uCD4xxY_400x400.jpg" alt="">
+        </div>
+    @else
+        <div class="img">
+            <img src="https://media.gqitalia.it/photos/5cc2c034d91c291ba14e9ec9/master/w_1600%2Cc_limit/Meme%2520Terrapiattisti4.png" alt="">
+        </div>
+    @endif
+
+        <ul>
+            @foreach ($informazioni as $info)
+                <li> {{ $info }} </li>
+            @endforeach
+        </ul>
+
 </body>
 
 </html>
--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     $saluto = "Hello World";
-    return view('home', compact('saluto'));
+    $stampa_immagine = true;
+    $informazioni = ['8 Miliardi di abitanti', 'Massa: 5,972 × 10^24 kg', 'Età: 4,543 × 10^9 anni', 'Gravità: 9,807 m/s²', 'Raggio: 6.371 km', 'Distanza da Marte: 1,698 unità astronomiche'];
+    return view('home', compact('saluto', 'stampa_immagine', 'informazioni'));
 
     return view('home');
 });
